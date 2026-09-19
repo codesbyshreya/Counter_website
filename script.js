47a6572dad055acf0b22c80d365c1e59551b61f9
Shreya Yadav <267423381+codesbyshreya@users.noreply.github.com>
2026-09-19 22:32:16 +0530

    Updated Counter to Basic Mantra tracker

--- script.js
+++ script.js
@@ -1,72 +1,62 @@
-let addbtn=document.querySelector("#buttonadd");
-let subbtn=document.querySelector("#buttonsub");
-let resetbtn=document.querySelector("#reset");
-let count=document.querySelector("#total-count")
-let togglebtn=document.querySelector("#toggle");
+let addbtn = document.querySelector("#buttonadd");
+let resetbtn = document.querySelector("#reset");
+let count = document.querySelector("#user-count")
+let togglebtn = document.querySelector("#toggle");
+let messagedisplay= document.querySelector("#message");
 
-let isDark=true;
+let isDark = true;
+let value = 0;
+let target = 108;
 
-togglebtn.addEventListener("click", function toggle(){
+localStorage.getItem("value", value);
+
+togglebtn.addEventListener("click", function toggle() {
     main = document.querySelector(".main");
-    if(isDark){
+    if (isDark) {
         main.style.backgroundColor = "white";
-        main.style.color = "black";
+        main.style.color = "#0B0F0F";
         isDark = false;
         togglebtn.textContent = "Dark Mode";
     } else {
-        main.style.backgroundColor = "black";
+        main.style.backgroundColor = "#0B0F0F";
         main.style.color = "white";
         isDark = true;
         togglebtn.textContent = "Light Mode";
     }
 });
 
-let value=0;
 
-addbtn.addEventListener("click", function add(){
-    //  if(value >= 10){
-    //     warning.style.display = "block";
-    //     return;
-    // }else{
-    //     warning.style.display = "none";
-    // }
-    value += 1;
-    count.textContent = value;
-    count.style.boxShadow = "0px 0px 100px #1df10596";
-    updateColor();
-})
-
-subbtn.addEventListener("click", function sub(){
-//    if(value <= -10){
-//         warning.style.display = "block";
-//         return;
-//     }else{
-//         warning.style.display = "none";
-//     }
-    value -= 1;
-    count.textContent = value;
-    count.style.boxShadow = "0px 0px 100px #f1340596";
-    updateColor();
-})
-
-resetbtn.addEventListener("click", function reset(){
-    value = 0;
-    count.textContent = value;
-    count.style.boxShadow = "0px 0px 100px #68c3bb96";
-    updateColor();
-})
-
-function updateColor(){
-    if (value > 0) {
-        count.style.color = "green";
-    } else if (value < 0) {
-        count.style.color = "red";
+addbtn.addEventListener("click", function add() {
+    if (value < target) {
+        localStorage.setItem("value",value+=1);
+        count.textContent = value;
+        count.style.boxShadow = "0px 0px 100px #1df10596";
     } else {
-        count.style.color = "white";
+        messagedisplay.textContent="Target reached!"
+        messagedisplay.style.display= "block";
+    }
+    updateColor();
+})
+
+resetbtn.addEventListener("click", function reset() {
+    let answer = confirm("Are you Sure You want to reset?");
+    if (answer == true) {
+        value = 0;
+        count.textContent = value;
+        count.style.boxShadow = "0px 0px 100px #68c3bb96";
+        updateColor();
+
+        if(messagedisplay.style.display!=="none"){
+            messagedisplay.style.display= "none  ";
+        }
+    }
+})
+
+function updateColor() {
+    if((value==target) || (value==0)){
+        count.style.color="white";
+    } else {
+        count.style.color="green";
     }
 }
 
-
-
-
-    
